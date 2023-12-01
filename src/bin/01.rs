82fc679543c74556b6c5148219f3467816c1e072
Sirio Negri <sirio.negri@ssd.it>
2023-12-01 23:29:35 +0100

    01-2 wip

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -12,10 +12,8 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
         }
         let string: String = format!("{}{}", numbers[0], numbers.last().unwrap());
-        println!("{}", string);
         sum += string.parse::<u32>().unwrap();
     }
-    println!("{}", sum);
     Some(sum)
 }
 
@@ -23,103 +21,44 @@ pub fn part_two(input: &str) -> Option<u32> {
     let splitted_input: Vec<&str> = input.lines().collect();
     let mut sum = 0;
     for text in splitted_input {
-        let mut numbers = String::new();
-        let mut number_1: char = 'a';
-        let mut number_2: char = 'a';
-        let mut index_1: u32;
-        let mut index_2: u32;
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
+        let mut numbers = Vec::new();
+        let mut positions = Vec::new();
+        let mut new_text = text.to_string();
+
+        let words = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
+        for (i, word) in words.iter().enumerate() {
+            while let Some(start) = new_text.find(word) {
+                numbers.push((i as u32).to_string());
+                positions.push(start);
+                new_text.remove(start + 1);
             }
         }
-        if text.contains("two") {
-            if number_1 == 'a' {
-                number_1 = '2';
-                index_1 = text.find("two").unwrap() as u32;
-            } else {
-                number_2 = "2";
-                index_2 = text.find("two").unwrap() as u32;
+        let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        for (i, digit) in digits.iter().enumerate() {
+            while let Some(start) = new_text.find(digit) {
+                numbers.push((i as u32).to_string());
+                positions.push(start);
+                new_text.remove(start);
             }
         }
-        if text.contains("three") {
-            if number_1 == 'a' {
-                number_1 = '2';
-                index_1 = text.find("three").unwrap() as u32;
-            } else {
-                number_2 = "";
-                index_2 = text.find("three").unwrap() as u32;
-            }
+        let mut permutation: Vec<usize> = (0..positions.len()).collect();
+        permutation.sort_by(|a, b| positions[*a].cmp(&positions[*b]));
+        let ordered_numbers: Vec<_> = permutation.iter().map(|i| &numbers[*i]).collect();
+        let mut string = String::new();
+        if ordered_numbers.len() == 1 {
+            string = format!("{}", ordered_numbers[0]);
+        } else if ordered_numbers.len() == 0 {
+            println!("posizione vuota");
+        } else {
+            string = format!("{}{}", ordered_numbers[0], ordered_numbers.last().unwrap());
+            //println!("{}", string);
         }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
-        if text.contains("one") {
-            if number_1 == 'a' {
-                number_1 = '1';
-                index_1 = text.find("one").unwrap() as u32;
-            } else {
-                number_2 = "1";
-                index_2 = text.find("one").unwrap() as u32;
-            }
-        }
+        //println!("{:?}\n{}", text, string);
+        sum += string.parse::<u32>().unwrap();
     }
-    None
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -134,7 +73,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
         assert_eq!(result, None);
     }
 }
