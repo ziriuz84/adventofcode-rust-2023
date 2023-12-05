60a6041650981fb2943e370ab58e48129446b675
Sirio Negri <ziriuz84@gmail.com>
2023-12-05 07:37:26 +0100

    Day 5-1 wip

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -11,6 +11,14 @@ pub struct Map {
 pub fn part_one(input: &str) -> Option<u32> {
     let splitted_input: Vec<&str> = input.split("\n\n").collect();
     println!("{:?}", splitted_input);
+    let seeds_input = splitted_input[0];
+    let seed_to_soil_input: Vec<&str> = splitted_input[1].split('\n').collect();
+    let soil_to_fertilizer_input: Vec<&str> = splitted_input[2].split('\n').collect();
+    let fertilizer_to_water_input: Vec<&str> = splitted_input[3].split('\n').collect();
+    let water_to_light_input: Vec<&str> = splitted_input[4].split('\n').collect();
+    let light_to_temperature_input: Vec<&str> = splitted_input[5].split('\n').collect();
+    let temperature_to_humidity_input: Vec<&str> = splitted_input[6].split('\n').collect();
+    let humidity_to_location_input: Vec<&str> = splitted_input[7].split('\n').collect();
     None
 }
 
