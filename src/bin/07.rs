8e88f82417ae20db833dc1345de8762277dc80b2
Sirio Negri <sirio.negri@ssd.it>
2023-12-11 19:06:12 +0100

    Day 7-1 WIP

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,14 +1,15 @@
+use std::cmp::Ordering;
 use std::collections::HashMap;
 advent_of_code::solution!(7);
 
 #[derive(Debug)]
 pub struct Hand<'a> {
     plays: &'a str,
-    bid: u32,
+    bid: u64,
     hand_type: HandType,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Clone, Copy)]
 enum HandType {
     HighCard,
     Pair,
@@ -19,7 +20,7 @@ enum HandType {
     FiveOfAKind,
 }
 
-pub fn get_card_value(c: char) -> u32 {
+pub fn get_card_value(c: char) -> u64 {
     match c {
         'A' => 14,
         'K' => 13,
@@ -38,16 +39,52 @@ pub fn get_card_value(c: char) -> u32 {
     }
 }
 
-pub fn get_hand_type(cards: &str) -> HandType {
-    let mut counts: HashMap<char, u32> = HashMap::new();
+impl<'a> Ord for Hand<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let strenghts = [
+            '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'T', 'Q', 'K', 'A',
+        ];
+        match self.hand_type.cmp(&other.hand_type) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Equal => {
+                let mut self_strengths = Vec::new();
+                let mut other_strengths = Vec::new();
+                for c in self.plays.chars() {
+                    self_strengths.push(strenghts.iter().position(|&ch| ch == c).unwrap());
+                }
+                for c in other.plays.chars() {
+                    other_strengths.push(strenghts.iter().position(|&ch| ch == c).unwrap());
+                }
+                self_strengths.iter().cmp(other_strengths.iter())
+            }
+        }
+    }
+}
+
+impl<'a> PartialOrd for Hand<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl<'a> Eq for Hand<'a> {}
+
+impl<'a> PartialEq for Hand<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        *self.plays == *other.plays
+    }
+}
+
+fn get_hand_type(cards: &str) -> HandType {
+    let mut counts: HashMap<char, u64> = HashMap::new();
     for c in cards.chars() {
         *counts.entry(c).or_insert(0) += 1;
     }
     let first = cards.chars().next().unwrap();
     if counts.len() == 1 {
-        return HandType::FiveOfAKind;
+        HandType::FiveOfAKind
     } else if counts.len() == 2 {
-        if vec![1, 4].contains(&counts[&first]) {
+        if [1, 4].contains(&counts[&first]) {
             return HandType::FourOfAKind;
         } else {
             return HandType::FullHouse;
@@ -64,27 +101,34 @@ pub fn get_hand_type(cards: &str) -> HandType {
     } else {
         return HandType::HighCard;
     }
-    return HandType::HighCard;
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let splitted_input: Vec<&str> = input.lines().collect();
-    println!("{:?}", splitted_input);
     let mut cards: Vec<Hand> = Vec::new();
-    let sum = 0;
     for text in splitted_input {
         let temp_text: Vec<&str> = text.split_whitespace().collect();
         cards.push(Hand {
             plays: temp_text[0],
-            bid: temp_text[1].parse::<u32>().unwrap(),
+            bid: temp_text[1].parse::<u64>().unwrap(),
             hand_type: get_hand_type(temp_text[0]),
         });
     }
-    println!("{:?}", cards);
+    cards.sort();
+    for card in cards {
+        println!("{:?}", card);
+    }
+    /*
+    let sum = cards
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, card)| acc + (i as u64 + 1) * card.bid);
     Some(sum)
+    */
+    None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
