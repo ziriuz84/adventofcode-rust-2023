6f7e3576e54ae0432bfb2a1661a821aa5d76bdae
Sirio Negri <ziriuz84@gmail.com>
2024-01-03 08:15:51 +0100

    day 12-1 wip

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -13,6 +13,61 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_part_one_a() {
+        let input = "?###???????? 3,2,1";
+        let result = part_one(&input);
+        assert_eq!(result, Some(10));
+    }
+
+    #[test]
+    fn test_part_one_b() {
+        let input = "?###???????? 3,2,1";
+        let result = part_one(&input);
+        assert_eq!(result, Some(10));
+    }
+
+    #[test]
+    fn test_part_one_c() {
+        let input = "?###???????? 3,2,1";
+        let result = part_one(&input);
+        assert_eq!(result, Some(10));
+    }
+
+    #[test]
+    fn test_part_one_d() {
+        let input = "?###???????? 3,2,1";
+        let result = part_one(&input);
+        assert_eq!(result, Some(10));
+    }
+
+    #[test]
+    fn test_part_one_e() {
+        let input = "?###???????? 3,2,1";
+        let result = part_one(&input);
+        assert_eq!(result, Some(10));
+    }
+
+    #[test]
+    fn test_part_one_f() {
+        let input = "?###???????? 3,2,1";
+        let result = part_one(&input);
+        assert_eq!(result, Some(10));
+    }
+
+    #[test]
+    fn test_part_one_total() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+";
+        let result = part_two(&input);
+        assert_eq!(result, Some(21));
+    }
+
+    #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
